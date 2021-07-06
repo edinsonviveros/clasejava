fff6b34aa420bdc94f40cd89d34b74b50fa63de1
edinsonviveros <ehviverosp@gmail.com>
2021-07-05 22:42:38 -0500

    Second class, java coding

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
 public class App {
+    
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private double estatura;
+    private float peso;
+    private boolean estadoCivil;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        // Tipos de datos y operadores
+        String res = "Hola";
+        int resultado = 15 + 32;
+        int resultado1 = 125 - 32;
+        float resultado2 = 58 * 21;
+        double resultado3 = 25 / 3;
+        int resultado4 = 21 % 9;
+        //Operadores unarios
+        resultado++; //resultado = resultado + 1
+        resultado--; //resultado = resultado - 1
+        resultado = -resultado;
+        resultado = +resultado;
+        //Operadores lógicos
+        boolean respuesta = true;
+        respuesta = 5 > 8;
+        respuesta = 5 < 8;
+        respuesta = 5 >= 8;
+        respuesta = 5 <= 8;
+        respuesta = 5 != 8;
+        respuesta = 5 == 8;
+
     }
 }
